23b7b074615e3761128cd182bcc5318dd9f143f8
João Eliandro <eliandrogermano@gmail.com>
2020-10-19 19:59:19 -0300

    Adjusted styles

---
+++ src/components/OrderCompleted/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export const ModalContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: #000;
+  opacity: 0.9;
+`;
+
+export const ModalText = styled.Text`
+  font-size: 24px;
+  font-family: 'Roboto';
+  line-height: 28px;
+  color: #fff;
+  font-weight: bold;
+  font-style: normal;
+  margin-top: 40px;
+`;
